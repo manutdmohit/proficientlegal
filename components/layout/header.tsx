0cd6651318d652071dc648e781a7791e7cedf1f6
manutdmohit <saudmohit@gmail.com>
2025-05-28 16:10:36 +0545

    Update Header component to hide the top info bar on mobile devices, ensuring a cleaner layout and improved user experience on smaller screens.

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -23,7 +23,7 @@ export function Header({
   return (
     <div ref={headerRef} className="fixed top-0 left-0 right-0 z-[100]">
       {/* Top info bar - Contains contact information and business hours */}
-      <div className="bg-[#003b73] text-white py-2 w-full">
+      <div className="hidden md:flex bg-[#003b73] text-white py-2 w-full">
         <div className="container px-4 md:px-6 mmx-auto w-full">
           {/* Remove mobile view from header - it will be in the floating panel */}
           <div className="hidden">
