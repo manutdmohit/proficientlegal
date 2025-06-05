63c4dba0f4903b5e7ba88cda00072a2ee8bef0de
manutdmohit <saudmohit@gmail.com>
2025-06-05 11:56:19 +0545

    Add POST endpoint to the post API route for submitting comments, including validation to prevent duplicate comments by the same email. Enhance error handling for internal server errors.

--- app/api/post/route.ts
+++ app/api/post/route.ts
@@ -3,19 +3,52 @@ import Post from '@/app/models/Post';
 import connectDB from '@/app/config/database';
 import mongoose from 'mongoose';
 
-// Add revalidation time of 1 hour
-export const revalidate = 3600;
-
 export async function GET(request: NextRequest) {
   try {
     await connectDB();
 
     const posts = await Post.find({});
 
-    // Add cache control headers
     const response = NextResponse.json(posts);
 
     return response;
+  } catch (error) {
+    return NextResponse.json(
+      { error: 'Internal server error' },
+      { status: 500 }
+    );
+  }
+}
+
+export async function POST(request: NextRequest) {
+  try {
+    await connectDB();
+
+    const { name, email, comment, postId } = await request.json();
+
+    const post = await Post.findOne({ _id: postId, 'comments.email': email });
+
+    if (post) {
+      return NextResponse.json(
+        { error: 'Comment already exists by this email' },
+        { status: 400 }
+      );
+    }
+
+    const newComment = {
+      name,
+      email,
+      comment,
+      createdAt: new Date(),
+    };
+
+    const updatedPost = await Post.findByIdAndUpdate(
+      postId,
+      { $push: { comments: newComment } },
+      { new: true }
+    );
+
+    return NextResponse.json(updatedPost);
   } catch (error) {
     return NextResponse.json(
       { error: 'Internal server error' },
